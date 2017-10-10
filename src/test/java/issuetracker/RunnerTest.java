9c1da5ccffee8c0fdb003efdf5357da9bcdf7dfd
Taranpreet Kohli <taranpreetkohli@yahoo.co.nz>
2017-10-10 15:22:38 +1300

    Add sample test case

---
+++ src/test/java/issuetracker/RunnerTest.java
@@ -0,0 +1,11 @@
+package issuetracker;
+
+import static org.junit.Assert.assertTrue;
+
+public class RunnerTest {
+
+    public void Runner_NoConditions_AssertsTrue() {
+        assertTrue(true);
+    }
+
+}
