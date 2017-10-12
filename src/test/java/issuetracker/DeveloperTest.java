e6e89b575a80d47d07cb079c1cbefde3001b048c
Taranpreet Kohli <taranpreetkohli@yahoo.co.nz>
2017-10-13 11:31:37 +1300

    Add developer test stubs

---
+++ src/test/java/issuetracker/DeveloperTest.java
@@ -0,0 +1,28 @@
+package issuetracker;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.junit.Test;
+
+public class DeveloperTest {
+
+    @Test
+    public void ResolveIssue_DeveloperAssignedToThatIssue_IssueResolved() {
+        throw new NotImplementedException("Stub!");
+    }
+
+    @Test
+    public void ResolveIssue_DeveloperAssignedToThatIssue_DevelopersListOfIssuesUpdated() {
+        throw new NotImplementedException("Stub!");
+    }
+
+    @Test
+    public void ResolveIssue_DeveloperNotAssignedToThatIssue_ExceptionThrown() {
+        throw new NotImplementedException("Stub!");
+    }
+
+    @Test
+    public void ResolveIssue_IssueDoesNotExist_ExceptionThrown() {
+        throw new NotImplementedException("Stub!");
+    }
+
+}
