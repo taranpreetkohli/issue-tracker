bcb06f0d69c3655c91e3be3621db95592d7217f0
Taranpreet Kohli <taranpreetkohli@yahoo.co.nz>
2017-10-13 10:30:00 +1300

    Add annotation tag to mock test

--- src/test/java/issuetracker/RunnerTest.java
+++ src/test/java/issuetracker/RunnerTest.java
@@ -1,9 +1,12 @@
 package issuetracker;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertTrue;
 
 public class RunnerTest {
 
+    @Test
     public void Runner_NoConditions_AssertsTrue() {
         assertTrue(true);
     }
