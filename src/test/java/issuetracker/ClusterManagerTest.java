1a925e1ca65f8d8c19291f2286e76a4898f0cd9c
Hailubby <hwan561@aucklanduni.ac.nz>
2017-10-13 13:08:54 +1300

    Add test stubs for ClusterManagerTest. Test for 5d not added

---
+++ src/test/java/issuetracker/ClusterManagerTest.java
@@ -0,0 +1,51 @@
+package issuetracker;
+
+import org.junit.*;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+
+public class ClusterManagerTest {
+    @BeforeClass
+    public static void beforeRun() {}
+
+    @AfterClass
+    public static void afterRun() {}
+
+    @Before
+    public void setUp() {}
+
+    @After
+    public void tearDown() {}
+
+    @Test
+    public void GroupForumPosts_SingleQuestion_ClusterTitleCorrectlySet() { throw new NotImplementedException(); }
+
+    @Test
+    public void GroupForumPosts_MultipleQuestions_ClusterTitleCorrectlySet() { throw new NotImplementedException(); }
+
+    @Test
+    public void GroupForumPosts_SingleQuestion_OneForumPostCorrectlyGrouped() { throw new NotImplementedException(); }
+
+    @Test
+    public void GroupForumPosts_MultipleQuestions_RelatedForumPostsCorrectlyGrouped() { throw new NotImplementedException(); }
+
+    @Test
+    public void GroupForumPosts_SingleQuestion_OnePost() { throw new NotImplementedException(); }
+
+    @Test
+    public void GroupForumPosts_MultipleQuestions_CorrectNumberOfPosts() { throw new NotImplementedException(); }
+
+    @Test
+    public void GroupForumPosts_SingleQuestion_OneUserAffected() { throw new NotImplementedException(); }
+
+    @Test
+    public void GroupForumPosts_MultipleQuestions_CorrectNumberOfUsersAffected() { throw new NotImplementedException(); }
+
+    @Test
+    public void GroupForumPosts_NoQuestions_ThrowException() { throw new NotImplementedException(); }
+
+    @Test
+    public void SummariseForumPosts_ValidCluster_CorrectSummaryCreated() { throw new NotImplementedException(); }
+
+
+}
