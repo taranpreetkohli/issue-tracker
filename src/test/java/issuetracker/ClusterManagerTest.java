4e2abaf3676f06d00d2e99d542973e920397674a
Hailubby <hwan561@aucklanduni.ac.nz>
2017-10-13 20:39:26 +1300

    Change method title test is for. Implement simple test for generating cluster titles

--- src/test/java/issuetracker/ClusterManagerTest.java
+++ src/test/java/issuetracker/ClusterManagerTest.java
@@ -1,12 +1,21 @@
 package issuetracker;
 
 import org.junit.*;
+import static org.junit.Assert.*;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import static org.mockito.Mockito.mock;
+
 public class ClusterManagerTest {
+    Cluster singlePostCluster;
+    Cluster multiplePostCluster;
+
     @BeforeClass
-    public static void beforeRun() {}
+    public static void beforeRun() {
+        singlePostCluster = mock(Cluster.class);
+        singlePostCluster = mock(Cluster.class);
+    }
 
     @AfterClass
     public static void afterRun() {}
@@ -18,17 +27,23 @@ public class ClusterManagerTest {
     public void tearDown() {}
 
     @Test
-    public void GroupForumPosts_SingleQuestion_ClusterTitleCorrectlySet() {
+    public void GenerateClusterTitle_SinglePostCluster_ClusterTitleCorrectlySet() {
         //arrange
         ClusterManager cm = new ClusterManager();
-        Cluster cluster = cm.groupForumPosts();
+        cm.generateClusterTitle(singlePostCluster);
 
         //assert
-        assertNotNull(cluster.getTitle());
+        assertTrue(!singlePostCluster.getTitle().isEmpty());
     }
 
     @Test
-    public void GroupForumPosts_MultipleQuestions_ClusterTitleCorrectlySet() { throw new NotImplementedException(); }
+    public void GenerateClusterTitle_MultiplePostCluster_ClusterTitleCorrectlySet() {
+        ClusterManager cm = new ClusterManager();
+        cm.generateClusterTitle(multiplePostCluster);
+
+        assertTrue(!multiplePostCluster.getTitle().isEmpty());
+
+    }
 
     @Test
     public void GroupForumPosts_SingleQuestion_OneForumPostCorrectlyGrouped() { throw new NotImplementedException(); }
