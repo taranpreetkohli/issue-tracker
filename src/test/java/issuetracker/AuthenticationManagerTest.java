a76f7eb89435cb22b465c3bd2df12f89d92782ed
Michael <kim.michael95@gmail.com>
2017-10-13 15:07:50 +1300

    Implement authentication tests

--- src/test/java/issuetracker/AuthenticationManagerTest.java
+++ src/test/java/issuetracker/AuthenticationManagerTest.java
@@ -1,10 +1,18 @@
 package issuetracker;
 
 import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
-import org.apache.commons.lang3.NotImplementedException;
 import org.junit.*;
 
 public class AuthenticationManagerTest {
+    private AuthenticationManager authManager;
+    private String newUsername;
+    private String newPassword;
+    private String newEmail;
+    private String existingUsername;
+    private String existingEmail;
+    private String existingPassword;
+
+
     @BeforeClass
     public static void beforeRun() {
     }
@@ -15,6 +23,15 @@ public class AuthenticationManagerTest {
 
     @Before
     public void setUp() {
+        authManager = new AuthenticationManager();
+
+        newUsername = "newUsername";
+        newEmail = "validEmail@gmail.com";
+        newPassword = "P4ssword";
+
+        existingUsername = "existingUsername";
+        existingEmail = "existingEmail@gmail.com";
+        existingPassword = "ex1stingPassword";
     }
 
     @After
@@ -22,47 +39,111 @@ public class AuthenticationManagerTest {
     }
 
     @Test
-    public void AddUser_NewUserUniqueId_UserObjectIsMade() {
-        throw new NotImplementedException("Stub!");
+    public void AddUser_NonExistantUsername_UserObjectIsMade() {
+        //Arrange
+        //Act
+        User user = authManager.addUser(newUsername, newPassword, newEmail);
+
+        //Assert
+        Assert.assertEquals(newUsername, user.getUsername());
+        Assert.assertEquals(newEmail, user.getEmail());
     }
 
     @Test
-    public void AddUser_UserNameSetMakesName_UserObjectIsMade() {
-        throw new NotImplementedException("Stub!");
+    public void AddUser_ExistingUsername_UserIsNotMadeAndExceptionIsThrown() {
+        //Arrange
+        //Act
+        authManager.addUser(existingUsername, newPassword, newEmail);
+
+        //Assert
+        Assert.fail();
     }
 
     @Test
-    public void AddUser_AdministratorUsersAreAdmins_UserObjectIsMade() {
-        throw new NotImplementedException("Stub!");
-    }
+    public void AddUser_ValidEmail_UserObjectIsMade() {
+        //Arrange
+        //Act
+        User user = authManager.addUser(newUsername, newPassword, newEmail);
 
-    @Test
-    public void AddUser_SetUserEmailIsSetCorrectly_UserObjectIsMade() {
-        throw new NotImplementedException("Stub!");
+        //Assert
+        Assert.assertEquals(newUsername, user.getUsername());
+        Assert.assertEquals(newEmail, user.getEmail());
     }
 
     @Test(expected = ValueException.class)
-    public void AddUser_MalformedEmail_UserIsNotMadeAndExceptionIsThrown() {
-        throw new NotImplementedException("Stub!");
+    public void AddUser_InvalidEmail_UserIsNotMadeAndExceptionIsThrown() {
+        //Arrange
+        String invalidEmail = "invalidEmail";
+
+        //Act
+        authManager.addUser(newUsername, newPassword, invalidEmail);
+
+        //Assert
     }
 
     @Test
-    public void LogIn_UserLogin_UserIsLoggedIn() {
-        throw new NotImplementedException("Stub!");
+    public void AddUser_ExistingEmail_UserIsNotMadeAndExceptionIsThrown() {
+        //Arrange
+        //Act
+        authManager.addUser(newUsername, newPassword, existingEmail);
+
+        //Assert
+        Assert.fail();
     }
 
     @Test
-    public void LogOut_UserLogout_UserLogsOut() {
-        throw new NotImplementedException("Stub!");
+    public void LogIn_ValidCredentials_UserIsLoggedIn() {
+        //Arrange
+
+        //Act
+        boolean successfulLogin = authManager.login(existingUsername, existingPassword);
+        User currentUser = authManager.getCurrentUser();
+
+        //Assert
+        Assert.assertTrue(successfulLogin);
+        Assert.assertEquals(existingUsername, currentUser.getUsername());
+        Assert.assertEquals(existingEmail, currentUser.getEmail());
     }
 
     @Test
     public void LogIn_InvalidUsernameDoesNotAllowLogin_UserIsUnableToLogin() {
-        throw new NotImplementedException("Stub!");
+        //Arrange
+        String invalidUsername = "invalid";
+
+        //Act
+        boolean successfulLogin = authManager.login(invalidUsername, "arbitraryPassword");
+
+        //Assert
+        Assert.assertFalse(successfulLogin);
     }
 
     @Test
     public void LogIn_InvalidPasswordDoesNotAllowLogin_UserIsUnableToLogin() {
-        throw new NotImplementedException("Stub!");
+        //Arrange
+        String wrongPassword = "wrongPassword";
+
+        //Act
+        boolean successfulLogin = authManager.login(existingUsername, wrongPassword);
+
+        //Assert
+        Assert.assertFalse(successfulLogin);
+    }
+
+    @Test
+    public void LogOut_UserLoggedIn_UserLogsOut() {
+        //Arrange
+        //Todo set up logged in state
+        //Act
+        authManager.logout();
+        //Assert
+    }
+
+    @Test
+    public void LogOut_NoUserLoggedIn_UserLogsOut() {
+        //Arrange
+        //Act
+        authManager.logout();
+        //Assert
+        Assert.fail();
     }
 }
