89efb64e8798839afaa61dad7e42cd9584029628
Taranpreet Kohli <taranpreetkohli@yahoo.co.nz>
2017-10-17 15:54:11 +1300

    Implement POJOs for User and Developer models

--- src/main/java/issuetracker/authentication/User.java
+++ src/main/java/issuetracker/authentication/User.java
@@ -5,6 +5,7 @@ import issuetracker.view.ICommand;
 import java.util.HashMap;
 
 public abstract class User {
+
     protected boolean loggedIn;
     private String email;
     private String password;
@@ -12,6 +13,10 @@ public abstract class User {
     public User(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+
+    public User() {
+
     }
 
     public abstract HashMap<String, ICommand> getView();
@@ -26,5 +31,17 @@ public abstract class User {
 
     public void setLoggedIn(boolean loggedIn){
         this.loggedIn = loggedIn;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
