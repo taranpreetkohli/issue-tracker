c29c3c3f582761e49770f60c296f70e825b974d8
Taranpreet Kohli <taranpreetkohli@yahoo.co.nz>
2017-10-17 16:06:09 +1300

    Remove Runner junk

--- src/main/java/issuetracker/Runner.java
+++ src/main/java/issuetracker/Runner.java
@@ -1,7 +1,5 @@
 package issuetracker;
 
-import issuetracker.authentication.User;
-import issuetracker.db.FirebaseAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +9,6 @@ public class Runner {
 
     public static void main(String... args) {
         System.out.println("Hello world!");
-        FirebaseAdapter db = new FirebaseAdapter();
-        User user = db.getUser("tkoh638@aucklanduni.ac.nz");
-        logger.error(user.getEmail());
-
-        User e = db.getUser("asdfasdf");
-        System.out.println(e + "asdfasdf");
     }
 
 }
