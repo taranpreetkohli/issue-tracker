52ac5d368d1e26aefaa919052307ac8b4c4a483a
Hailubby <hwan561@aucklanduni.ac.nz>
2017-10-19 11:42:36 +1300

    Remove cliche dependency and imports

--- src/main/java/issuetracker/Runner.java
+++ src/main/java/issuetracker/Runner.java
@@ -1,9 +1,5 @@
 package issuetracker;
 
-import asg.cliche.Command;
-import asg.cliche.Param;
-import asg.cliche.ShellFactory;
-import issuetracker.cli.CLIManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
