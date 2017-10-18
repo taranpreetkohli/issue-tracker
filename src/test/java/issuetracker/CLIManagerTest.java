50adc8c0f229e58213f1c432fbe2196825f1a3b3
Hailubby <hwan561@aucklanduni.ac.nz>
2017-10-18 14:19:55 +1300

    Add test stubs for CLIManagerTest

--- src/test/java/issuetracker/CLIManagerTest.java
+++ src/test/java/issuetracker/CLIManagerTest.java
@@ -1,11 +1,11 @@
 package issuetracker;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import issuetracker.cli.CLIManager;
+import org.apache.commons.lang3.NotImplementedException;
+import org.junit.*;
 
 public class CLIManagerTest {
+    private CLIManager cliManager;
     @BeforeClass
     public static void beforeRun() {
     }
@@ -15,9 +15,71 @@ public class CLIManagerTest {
     }
 
     @Before
-    public void setUp() {}
+    public void setUp() {
+        cliManager = new CLIManager();
+    }
 
     @After
     public void tearDown(){
+    }
+
+    @Test
+    public void CheckInputFormat_ValidInput_ReturnsTrue() {
+        //Arrange
+
+        //Act
+
+        //Assert
+        throw new NotImplementedException("Stub!");
+    }
+
+    @Test
+    public void CheckInputFormat_InvalidInput_ReturnsFalse() {
+        //Arrange
+
+        //Act
+
+        //Assert
+        throw new NotImplementedException("Stub!");
+    }
+
+    @Test
+    public void CheckInputFormat_NoInput_NoInputExceptionThrown() {
+        //Arrange
+
+        //Act
+
+        //Assert
+        throw new NotImplementedException("Stub!");
+    }
+
+    @Test
+    public void IsValidCommand_ValidCommand_ReturnsTrue() {
+        //Arrange
+
+        //Act
+
+        //Assert
+        throw new NotImplementedException("Stub!");
+    }
+
+    @Test
+    public void IsValidCommand_InvalidFormat_ReturnsFalse() {
+        //Arrange
+
+        //Act
+
+        //Assert
+        throw new NotImplementedException("Stub!");
+    }
+
+    @Test
+    public void IsValidCommand_NoCommand_NoInputExceptionThrown() {
+        //Arrange
+
+        //Act
+
+        //Assert
+        throw new NotImplementedException("Stub!");
     }
 }
