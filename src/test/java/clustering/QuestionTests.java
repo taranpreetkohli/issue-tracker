cf08a7fe8f8b3fd2126aa958351e6b7bd42816aa
Taranpreet Kohli <taranpreetkohli@yahoo.co.nz>
2017-10-18 16:44:18 +1300

    Write tests for Questions

--- src/test/java/clustering/QuestionTests.java
+++ src/test/java/clustering/QuestionTests.java
@@ -1,32 +1,39 @@
 package clustering;
 
 import issuetracker.clustering.Question;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.DataFormatException;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class QuestionTests {
 
-    private static String expectedARFF;
+    private static String singleQuestionExpectedARFF;
+    private static String doubleQuestionExpectedARFF;
     private static Question toBeParsed;
-    private Question q;
+
+    private Question questionOne;
+    private Question questionTwo;
 
     @BeforeClass
     public static void beforeClass() {
-        expectedARFF = "@relation TDD\n\n" +
+        singleQuestionExpectedARFF = "@relation questions\n\n" +
                 "@attribute questionID LONG\n" +
                 "@attribute question STRING\n" +
                 "@attribute date STRING\n" +
                 "@attribute author STRING\n" +
                 "@attribute forumID LONG\n" +
-                "@attribute information INFORMATION\n" +
+                "@attribute information STRING\n" +
                 "@attribute url STRING\n\n" +
                 "@data\n" +
                 "44330,\"xero-php API, how to structure query\",1/06/2017,Rajiv Pardiwala,2286,\"I'm try to request " +
@@ -36,85 +43,73 @@ public class QuestionTests {
                 " 00:00:00'))->execute());which becomes this:\"\"https://api.xero.com/api.xro/2.0/BankTransactions?" +
                 "AccountID=XXXXXXX-4097-4028-9b99-XXXXXXXXX&fromDate=2016-06-01\"\"however, it is not filtering by" +
                 " AccountID and instead returns transactions for both of my accounts.  The fromDate is ignored as well." +
-                "  What is the correct url to do what I want?\",https://community.xero.com/developer/discussion/50795838/";
+                "  What is the correct url to do what I want?\",https://community.xero.com/developer/discussion/50795838/" + "\n";
 
-//                "44330,\"xero-php API, how to structure query\",1/06/2017,Rajiv Pardiwala,2286,\"I'm try to request " +
-//                "bank transactions from a specific account from a certain date using the xero-php API.$xero = new " +
-//                "PrivateApplication($config);print_r(  $xero->load('Accounting\\BankTransaction')->" +
-//                "setParameter('AccountID','XXXXXXX-4097-4028-9b99-XXXXXXXXX')->fromDate(new DateTime('2016-06-01" +
-//                " 00:00:00'))->execute());which becomes this:\"\"https://api.xero.com/api.xro/2.0/BankTransactions?" +
-//                "AccountID=XXXXXXX-4097-4028-9b99-XXXXXXXXX&fromDate=2016-06-01\"\"however, it is not filtering by" +
-//                " AccountID and instead returns transactions for both of my accounts.  The fromDate is ignored as well." +
-//                "  What is the correct url to do what I want?\",https://community.xero.com/developer/discussion/50795838/";
+        doubleQuestionExpectedARFF = singleQuestionExpectedARFF +
+                "44331,Delivery Address on Invoice,31/05/2017,Sam Jones,2286,Hi,I am wondering if it is possible to add" +
+                " a Delivery Address to an invoice. We have suppliers with multiple, constantly in use, delivery address." +
+                " ,https://community.xero.com/developer/discussion/50739864/\n";
+
     }
 
     @Before
     public void setUp() throws Exception {
-        q = new Question();
-        q.setQuestionID(44330);
-        q.setQuestion("\"xero-php API, how to structure query\"");
-        q.setDate(new Date("1/06/2017"));
-        q.setAuthor("Rajiv Pardiwala");
-        q.setForumID(2286);
-        q.setInformation("\"I'm try to request " +
-                "bank transactions from a specific account from a certain date using the xero-php API.$xero = new " +
-                "PrivateApplication($config);print_r(  $xero->load('Accounting\\BankTransaction')->" +
-                "setParameter('AccountID','XXXXXXX-4097-4028-9b99-XXXXXXXXX')->fromDate(new DateTime('2016-06-01" +
-                " 00:00:00'))->execute());which becomes this:\"\"https://api.xero.com/api.xro/2.0/BankTransactions?" +
-                "AccountID=XXXXXXX-4097-4028-9b99-XXXXXXXXX&fromDate=2016-06-01\"\"however, it is not filtering by" +
-                " AccountID and instead returns transactions for both of my accounts.  The fromDate is ignored as well." +
-                "  What is the correct url to do what I want?\"");
-        q.setUrl("https://community.xero.com/developer/discussion/50795838/");
+        questionOne = new Question()
+                .setQuestionID(44330)
+                .setQuestion("\"xero-php API, how to structure query\"")
+                .setDate("1/06/2017")
+                .setAuthor("Rajiv Pardiwala")
+                .setForumID(2286)
+                .setInformation("\"I'm try to request " +
+                        "bank transactions from a specific account from a certain date using the xero-php API.$xero = new " +
+                        "PrivateApplication($config);print_r(  $xero->load('Accounting\\BankTransaction')->" +
+                        "setParameter('AccountID','XXXXXXX-4097-4028-9b99-XXXXXXXXX')->fromDate(new DateTime('2016-06-01" +
+                        " 00:00:00'))->execute());which becomes this:\"\"https://api.xero.com/api.xro/2.0/BankTransactions?" +
+                        "AccountID=XXXXXXX-4097-4028-9b99-XXXXXXXXX&fromDate=2016-06-01\"\"however, it is not filtering by" +
+                        " AccountID and instead returns transactions for both of my accounts.  The fromDate is ignored as well." +
+                        "  What is the correct url to do what I want?\"")
+                .setUrl("https://community.xero.com/developer/discussion/50795838/");
+
+        questionTwo = new Question()
+                .setQuestionID(44331)
+                .setQuestion("Delivery Address on Invoice")
+                .setAuthor("Sam Jones")
+                .setDate("31/05/2017")
+                .setForumID(2286)
+                .setInformation("Hi,I am wondering if it is possible to add a Delivery Address to an invoice. We have suppliers with multiple, constantly in use, delivery address. ")
+                .setUrl("https://community.xero.com/developer/discussion/50739864/");
     }
 
     @Test
     public void toARFF_SingleQuestion_MakesARFFString() {
         // Arrange
-        String actual = "";
+        String actual;
         // Act
-        actual = q.toARFF();
+        actual = questionOne.toARFF();
         // Assert
-        Assert.assertEquals(expectedARFF, actual);
+        Assert.assertEquals(singleQuestionExpectedARFF, actual);
     }
 
-    @Ignore
-    //TODO This is so much work to test someone else can do this.
     @Test
     public void toARFF_MultipleQuestions_MakesARFFString() {
         // Arrange
         String actual = "";
-        String expected = "@relation TDD\n\n" +
-                "@attribute questionID LONG\n" +
-                "@attribute question STRING\n" +
-                "@attribute date STRING\n" +
-                "@attribute author STRING\n" +
-                "@attribute forumID LONG\n" +
-                "@attribute information INFORMATION\n" +
-                "@attribute url STRING\n\n" +
-                "@data\n" +
-                "44330,\"xero-php API, how to structure query\",1/06/2017,Rajiv Pardiwala,2286,\"I'm try to request " +
-                "bank transactions from a specific account from a certain date using the xero-php API.$xero = new " +
-                "PrivateApplication($config);print_r(  $xero->load('Accounting\\BankTransaction')->" +
-                "setParameter('AccountID','XXXXXXX-4097-4028-9b99-XXXXXXXXX')->fromDate(new DateTime('2016-06-01" +
-                " 00:00:00'))->execute());which becomes this:\"\"https://api.xero.com/api.xro/2.0/BankTransactions?" +
-                "AccountID=XXXXXXX-4097-4028-9b99-XXXXXXXXX&fromDate=2016-06-01\"\"however, it is not filtering by" +
-                " AccountID and instead returns transactions for both of my accounts.  The fromDate is ignored as well." +
-                "  What is the correct url to do what I want?\",https://community.xero.com/developer/discussion/50795838/\n" +
-                "44331,Delivery Address on Invoice,31/05/2017,Sam Jones,2286,\"Hi,I am wondering if it is possible to add a Delivery Address to an invoice. We have suppliers with multiple, constantly in use, delivery address. We need to add this address onto the invoice in some way so the supplier knows what address it was for.I know in the docs it states \"\"It is not possible to manually override the address on a per-invoice basis.\"\" but does anyone know a way around this or an alternative solution to record the address on an invoice?ThanksSam\",https://community.xero.com/developer/discussion/50739864/";
-        String parsed = toBeParsed + "\n" + "44331,Delivery Address on Invoice,31/05/2017,Sam Jones,2286,\"Hi,I am wondering if it is possible to add a Delivery Address to an invoice. We have suppliers with multiple, constantly in use, delivery address. We need to add this address onto the invoice in some way so the supplier knows what address it was for.I know in the docs it states \"\"It is not possible to manually override the address on a per-invoice basis.\"\" but does anyone know a way around this or an alternative solution to record the address on an invoice?ThanksSam\",https://community.xero.com/developer/discussion/50739864/";
-        // Act
-//        actual = parser.parse(parsed);
+        List<Question> questionList = new ArrayList<>();
+        questionList.add(questionOne);
+        questionList.add(questionTwo);
+
+        actual = Question.toARFF(questionList);
         // Assert
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(doubleQuestionExpectedARFF, actual);
     }
 
     @Test(expected = DataFormatException.class)
     public void toARFF_MalformedQuestion_ThrowsFormatException() {
         // Arrange
         String actual = "";
-        Question toConvert = spy(q);
-        when(q.getAuthor()).thenReturn(null);
-        when(q.getInformation()).thenReturn(null);
+        Question toConvert = spy(questionOne);
+        when(questionOne.getAuthor()).thenReturn(null);
+        when(questionOne.getInformation()).thenReturn(null);
         // Act
         actual = toConvert.toARFF();
         // Assert
