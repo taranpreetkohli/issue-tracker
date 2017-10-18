30efe6440852375d44b9cf3ff67ad6ab23bcbfa5
Hailubby <hwan561@aucklanduni.ac.nz>
2017-10-18 13:44:54 +1300

    Create CLI Manager test skeleton

---
+++ src/test/java/issuetracker/CLIManagerTest.java
@@ -0,0 +1,23 @@
+package issuetracker;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public class CLIManagerTest {
+    @BeforeClass
+    public static void beforeRun() {
+    }
+
+    @AfterClass
+    public static void afterRun() {
+    }
+
+    @Before
+    public void setUp() {}
+
+    @After
+    public void tearDown(){
+    }
+}
