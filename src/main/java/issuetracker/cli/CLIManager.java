b692444b26c89a3289fb19bc4a9cced1b4c90a95
Hailubby <hwan561@aucklanduni.ac.nz>
2017-10-19 11:49:17 +1300

    modify implementation of isValidCommand to pass new tests

--- src/main/java/issuetracker/cli/CLIManager.java
+++ src/main/java/issuetracker/cli/CLIManager.java
@@ -1,19 +1,30 @@
 package issuetracker.cli;
 
+import issuetracker.authentication.AuthenticationManager;
+import issuetracker.clustering.ClusterManager;
 import issuetracker.exception.NoInputException;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CLIManager {
+    private AuthenticationManager authenticationManager;
+    private ClusterManager clusterManager;
 
-    public void startCLI() {}
+    public CLIManager(AuthenticationManager authenticationManager, ClusterManager clusterManager) {
+        this.authenticationManager = authenticationManager;
+        this.clusterManager = clusterManager;
+    }
 
-    public boolean isValidCommand(String command, HashMap view) {
+    public void startCLI() {
+    }
+
+    public boolean isValidCommand(String command) {
         if (command.isEmpty()){
             throw new NoInputException("No command given");
         }
 
-        if (view.keySet().contains(command)){
+        if (authenticationManager.getCurrentUser().getView().keySet().contains(command)){
             return true;
         } else {
             return false;
