5972020bc3f63b7f3369305b2fe2a96944eb6059
Michael <kim.michael95@gmail.com>
2017-10-18 16:08:18 +1300

    Implement the checkUserDetailsFormat method in CLIManager.
    Mob Programmed With:
    - John Ramirez
    - Hailun Wang

--- src/main/java/issuetracker/cli/CLIManager.java
+++ src/main/java/issuetracker/cli/CLIManager.java
@@ -1,10 +1,22 @@
 package issuetracker.cli;
 
+import issuetracker.exception.NoInputException;
+
 public class CLIManager {
 
     public boolean isValidCommand(String command) { return false; }
 
     public boolean checkUserDetailFormat(String validInput) {
-        return false;
+
+        if (validInput.isEmpty()){
+            throw new NoInputException("No Email or Password Provided");
+        }
+
+        String[] parts = validInput.split(" ");
+        if (parts.length == 2){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
