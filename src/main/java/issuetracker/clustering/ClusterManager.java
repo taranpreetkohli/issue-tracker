c44152ec8062a4aea6245e9cdd38fff7f98a603b
Taranpreet Kohli <taranpreetkohli@yahoo.co.nz>
2017-10-18 16:58:30 +1300

    Remove DBScan scratch code

--- src/main/java/issuetracker/clustering/ClusterManager.java
+++ src/main/java/issuetracker/clustering/ClusterManager.java
@@ -2,38 +2,29 @@ package issuetracker.clustering;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import weka.clusterers.DBSCAN;
-import weka.core.Instances;
-import weka.filters.Filter;
-import weka.filters.unsupervised.attribute.StringToWordVector;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.net.URL;
 
 public class ClusterManager {
 
     private static Logger logger = LoggerFactory.getLogger(ClusterManager.class);
 
     public ClusterManager() {
-        try {
-            URL testResourceURL = getClass().getClassLoader().getResource("test.arff");
-            File testFile = new File(testResourceURL.getFile());
-            Instances instances = new Instances(new BufferedReader(new FileReader(testFile)));
-            StringToWordVector s = new StringToWordVector();
-            s.setInputFormat(instances);
-            instances = Filter.useFilter(instances, s);
-            DBSCAN dbscan = new DBSCAN();
-            System.out.println(instances.toString());
-            dbscan.setEpsilon(1);
-            dbscan.setMinPoints(1);
-            dbscan.buildClusterer(instances);
-            System.out.println(dbscan.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
-        }
+//        try {
+//            URL testResourceURL = getClass().getClassLoader().getResource("test.arff");
+//            File testFile = new File(testResourceURL.getFile());
+//            Instances instances = new Instances(new BufferedReader(new FileReader(testFile)));
+//            StringToWordVector s = new StringToWordVector();
+//            s.setInputFormat(instances);
+//            instances = Filter.useFilter(instances, s);
+//            DBSCAN dbscan = new DBSCAN();
+//            System.out.println(instances.toString());
+//            dbscan.setEpsilon(1);
+//            dbscan.setMinPoints(1);
+//            dbscan.buildClusterer(instances);
+//            System.out.println(dbscan.toString());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException(e.getMessage());
+//        }
     }
 
     public Cluster generateCluster(String input) {
