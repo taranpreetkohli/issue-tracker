575e3c3ca7e2d9907b6eca213064a114b8c73f0a
Akram <akrdar@gmail.com>
2017-10-20 12:43:01 +1300

    Attempt to train algorithm

--- src/main/java/issuetracker/clustering/IssueManager.java
+++ src/main/java/issuetracker/clustering/IssueManager.java
@@ -1,7 +1,20 @@
 package issuetracker.clustering;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IssueManager {
 
@@ -25,6 +38,36 @@ public class IssueManager {
 //            e.printStackTrace();
 //            throw new RuntimeException(e.getMessage());
 //        }
+
+
+        Map<Integer, String> trainingData = new HashMap<>();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(getClass().getClassLoader().getResource("training_data_content.txt").getFile()));
+            String currentLine;
+            while ((currentLine = br.readLine()) != null) {
+                String[] values = currentLine.split("\\t");
+                trainingData.put(Integer.parseInt(values[0]), String.join(" ", Arrays.copyOfRange(values, 1, values.length)));
+            }
+//            for (int i : trainingData.keySet()) {
+//                System.out.println(trainingData.get(i));
+//            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FastVector training = new FastVector();
+        training.addElement(new Attribute("dunno", new FastVector()));
+
+        Instances dataSet = new Instances("Training_Data", training, 1000);
+        Instance inst = new Instance(1);
+        Attribute a1 = dataSet.attribute("dunno");
+        for (int i : trainingData.keySet()) {
+            inst.setValue(a1, trainingData.get(i));
+            dataSet.add(inst);
+        }
+
+        System.out.println(dataSet.toString());
+
     }
 
     public Issue generateCluster(String input) {
