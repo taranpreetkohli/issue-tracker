78128296d0d772606e8e3ea1233b529407a6ea9f
Hailubby <hwan561@aucklanduni.ac.nz>
2017-10-20 11:30:07 +1300

    Create method stub to remove compiler errors
    Pairprogrammed with John

--- src/main/java/issuetracker/cli/view/Command.java
+++ src/main/java/issuetracker/cli/view/Command.java
@@ -1,5 +1,12 @@
 package issuetracker.cli.view;
 
+import issuetracker.authentication.AuthenticationManager;
+import issuetracker.cli.CLIManager;
+
 public class Command implements ICommand {
 
+    @Override
+    public void run(AuthenticationManager authenticationManager, String userInput, CLIManager cliManager) {
+
+    }
 }
--- src/main/java/issuetracker/cli/view/ICommand.java
+++ src/main/java/issuetracker/cli/view/ICommand.java
@@ -1,4 +1,9 @@
 package issuetracker.cli.view;
 
+import issuetracker.authentication.AuthenticationManager;
+import issuetracker.cli.CLIManager;
+
 public interface ICommand {
+
+    void run(AuthenticationManager authenticationManager, String userInput, CLIManager cliManager);
 }
