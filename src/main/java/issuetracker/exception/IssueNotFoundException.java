9861869551866838b80e38a72914f71597842c2f
Akram <akrdar@gmail.com>
2017-10-21 13:34:57 +1300

    Add no parameter constructor for IssueNotFoundExceptions

--- src/main/java/issuetracker/exception/IssueNotFoundException.java
+++ src/main/java/issuetracker/exception/IssueNotFoundException.java
@@ -2,6 +2,7 @@ package issuetracker.exception;
 
 public class IssueNotFoundException extends RuntimeException {
 
+    public IssueNotFoundException(){}
     public IssueNotFoundException(String s){
         super(s);
     }
